4e71bef705ca1e96162ff00e42ce0801407b0bcd
Sidali Assoul <assoulsidali@gmail.com>
2023-10-08 15:45:23 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
+import svgrPlugin from 'vite-plugin-svgr';
+import {addAttributePlugin} from "@componly/vite-plugin"
+//@ts-ignore
+export default defineConfig(({ mode }) => {
+  const config = {
+    plugins: [addAttributePlugin(),react(), viteTsconfigPaths(), svgrPlugin()],
+    mode: 'production',
+    // define: {
+    //   __VITE_DEV_SERVER__: true, // Enable Vite dev server in production
+    // },
+    
+    build:{
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+      // rollupOptions: {
+      //   external: ['@mui/icons-material',"@mantine/core"] // Mark @mui/icons-material as external
+      // },
+      sourcemap: false,
+
+    }
+  };
+
+  return config;
+});
